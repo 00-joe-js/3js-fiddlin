1f40442435ba2be0bf906276e4e01cac54f4d0f5
Joe Alves <joseph.michael.alves@gmail.com>
2021-05-15 12:41:54 -0400

    <3

--- with-ts/src/config/index.ts
+++ with-ts/src/config/index.ts
@@ -2,7 +2,7 @@ export const MAIN_VIEWPORT_WIDTH = window.innerWidth;
 export const MAIN_VIEWPORT_HEIGHT = window.innerHeight;
 
 // Dippin Dots space
-export const DOTS_AMOUNT = 3000;
+export const DOTS_AMOUNT = 7777;
 export const CUBIC_SIDE = 100000;
 export const DOT_SIZE = 75;
 
